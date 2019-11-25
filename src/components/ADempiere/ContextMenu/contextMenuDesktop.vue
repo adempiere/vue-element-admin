c997fa27a4e0ef37759b8b0dd7c6ac3cf08fa939
leonel1524 <matosleonel0@gmail.com>
2019-11-25 18:10:28 -0400

    add const to static routes

--- src/components/ADempiere/ContextMenu/contextMenuDesktop.vue
+++ src/components/ADempiere/ContextMenu/contextMenuDesktop.vue
@@ -35,7 +35,7 @@
               {{ $t('components.contextMenuDownload') }}
             </a>
           </el-menu-item>
-          <el-menu-item v-show="$route.name === 'Report Viewer'" index="9" @click="$router.push({ name: '8e5131dc-fb40-11e8-a479-7a0060f0aa01' })">
+          <el-menu-item v-show="$route.name === 'Report Viewer'" index="9" @click="$router.push({ name: ROUTES.PRINT_FORMAT_SETUP_WINDOW.uuid })">
             {{ $t('components.contextMenuPrintFormatSetup') }}
           </el-menu-item>
           <el-menu-item v-if="getDataSelection.length > 0 && panelType === 'browser'" index="6" @click="exporBrowser">
