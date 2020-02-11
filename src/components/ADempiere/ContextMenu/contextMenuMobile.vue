a74916be130a07afb5f16cddd443269c898b89f6
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2020-02-11 16:45:38 -0400

    fix: Export record in table, and mobile support to Smart Browser.

--- src/components/ADempiere/ContextMenu/contextMenuMobile.vue
+++ src/components/ADempiere/ContextMenu/contextMenuMobile.vue
@@ -11,7 +11,7 @@
         unique-opened
         style="width: 258px; float: right;"
       >
-        <el-submenu index="1">
+        <el-submenu index="relations">
           <template slot="title">
             <svg-icon icon-class="tree" />
             {{ $t('components.contextMenuRelations') }}
@@ -22,7 +22,8 @@
             </el-scrollbar>
           </el-menu-item-group>
         </el-submenu>
-        <el-submenu index="2">
+
+        <el-submenu index="actions">
           <template slot="title">
             <svg-icon icon-class="link" />
             {{ $t('components.contextMenuActions') }}
@@ -59,21 +60,38 @@
                   {{ action.name }}
                 </el-menu-item>
               </template>
-              <el-menu-item v-show="isReport" index="4">
+              <!-- other actions -->
+              <el-menu-item v-show="isReport" index="downloadReport">
                 <a :href="downloads" :download="file">
                   {{ $t('components.contextMenuDownload') }}
                 </a>
               </el-menu-item>
-              <el-menu-item index="5" @click="setShareLink">
-                {{ $t('components.contextMenuShareLink') }}
+              <el-submenu
+                v-if="['browser', 'window'].includes(panelType)"
+                :disabled="panelType === 'browser' && getDataSelection.length < 1"
+                index="exportRecord"
+              >
+                <template slot="title">
+                  {{ $t('data.exportRecord') }}
+                </template>
+                <el-menu-item v-for="(format, keyFormat) in supportedTypes" :key="keyFormat" :index="keyFormat" @click.native="exportRecord(keyFormat)">
+                  {{ format }}
+                </el-menu-item>
+              </el-submenu>
+              <el-menu-item v-show="$route.name === 'Report Viewer'" index="printFormat" @click="redirect">
+                {{ $t('components.contextMenuPrintFormatSetup') }}
               </el-menu-item>
-              <el-menu-item index="6" @click="refreshData">
+              <el-menu-item v-if="panelType !== 'process'" index="refreshData" @click="refreshData">
                 {{ $t('components.contextMenuRefresh') }}
+              </el-menu-item>
+              <el-menu-item index="shareLink" @click="setShareLink">
+                {{ $t('components.contextMenuShareLink') }}
               </el-menu-item>
             </el-scrollbar>
           </el-menu-item-group>
         </el-submenu>
-        <el-submenu :disabled="!(isReferecesContent && isLoadedReferences)" class="el-menu-item" index="3">
+
+        <el-submenu :disabled="!(isReferecesContent && isLoadedReferences)" class="el-menu-item" index="references">
           <template slot="title">
             {{ $t('components.contextMenuReferences') }}
           </template>
