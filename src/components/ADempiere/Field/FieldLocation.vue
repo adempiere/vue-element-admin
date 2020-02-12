5a73027e6c04d4623ee73f450e9e7ee2fc81bb80
leonel1524 <matosleonel0@gmail.com>
2020-02-12 16:48:51 -0400

    redefine call data

--- src/components/ADempiere/Field/FieldLocation.vue
+++ src/components/ADempiere/Field/FieldLocation.vue
@@ -4,10 +4,12 @@
     v-model="value"
     :placeholder="metadata.help"
     :options="options"
-    :props="{ expandTrigger: 'hover' }"
+    :props="props"
     :readonly="Boolean(metadata.readonly)"
     :disabled="isDisabled"
     filterable
+    @visible-change="getParentList"
+    @expand-change="getChildList"
     @change="preHandleChange"
   />
 </template>
@@ -21,44 +23,122 @@ export default {
   data() {
     return {
       value: [],
-      options: []
+      options: [],
+      props: { checkStrictly: true }
     }
   },
-  mounted() {
-    this.$store.dispatch('getObjectListFromCriteria', {
-      tableName: 'C_Country',
-      orderByClause: 'C_Country_ID'
-    })
-      .then(response => {
-        var countryMap = response.map(item => {
-          return {
-            label: item.Name,
-            value: item.C_Country_ID,
-            hasRegion: item.HasRegion,
-            children: []
-          }
+  watch: {
+    'metadata.value'(value) {
+      if (!this.isEmptyValue(value)) {
+        this.getValueFromServer(value)
+      }
+    }
+  },
+  methods: {
+    preHandleChange() {
+      const selected = this.value[this.value.length - 1]
+      this.handleChange(selected)
+    },
+    getParentList(isShowed) {
+      if (isShowed) {
+        this.$store.dispatch('getObjectListFromCriteria', {
+          tableName: 'C_Country',
+          orderByClause: 'C_Country_ID',
+          isShowNotification: false
         })
-        countryMap.forEach(country => {
-          if (country.hasRegion) {
-            this.$store.dispatch('getObjectListFromCriteria', {
-              tableName: 'C_Region',
-              whereClause: `C_Country_ID = ${country.value}`
+          .then(response => {
+            const countryMap = response.map(item => {
+              if (item.HasRegion) {
+                item.children = []
+              }
+              return {
+                label: item.Name,
+                value: item.C_Country_ID,
+                hasRegion: item.HasRegion,
+                children: item.children
+              }
             })
-              .then(response => {
-                country.children = response.map(item => {
+            this.options = countryMap
+          })
+      }
+    },
+    getChildList(parent) {
+      if (parent.length > 1) {
+        const childId = parent[1]
+        const parentId = parent[0]
+        const parentOption = this.options.find(country => country.value === parentId)
+        const childsOptions = parentOption.children.find(region => region.value === childId)
+        this.$store.dispatch('getObjectListFromCriteria', {
+          tableName: 'C_City',
+          whereClause: `C_Region_ID = ${childId}`,
+          isShowNotification: false
+        })
+          .then(response => {
+            if (response.length) {
+              childsOptions.children = response.map(item => {
+                return {
+                  label: item.Name,
+                  value: item.C_Region_ID,
+                  country: parentOption.value
+                }
+              })
+            } else {
+              delete childsOptions.children
+            }
+          })
+      } else {
+        const parentId = parent[0]
+        const parentOption = this.options.find(country => country.value === parentId)
+        if (parentOption && parentOption.hasRegion) {
+          this.$store.dispatch('getObjectListFromCriteria', {
+            tableName: 'C_Region',
+            whereClause: `C_Country_ID = ${parentId}`,
+            isShowNotification: false
+          })
+            .then(response => {
+              if (response.length) {
+                parentOption.children = response.map(item => {
                   return {
                     label: item.Name,
                     value: item.C_Region_ID,
-                    country: country.value
+                    country: parentOption.value,
+                    children: []
                   }
                 })
+              }
+            })
+        }
+      }
+    },
+    getValueFromServer(value) {
+      this.$store.dispatch('getObjectListFromCriteria', {
+        tableName: 'C_Region',
+        whereClause: `C_Region_ID=${value}`,
+        isShowNotification: false
+      })
+        .then(response => {
+          if (response.length) {
+            const record = response[0]
+            this.$store.dispatch('getObjectListFromCriteria', {
+              tableName: 'C_Country',
+              whereClause: `C_Country_ID=${record.C_Country_ID}`,
+              isShowNotification: false
+            })
+              .then(responseCountry => {
+                this.options.push({
+                  value: record.C_Country_ID,
+                  label: responseCountry[0].Name,
+                  children: [{
+                    value: record.C_Region_ID,
+                    label: record.Name
+                  }]
+                })
+                this.value.push(response[0].C_Country_ID)
+                this.value.push(response[0].C_Region_ID)
               })
-          } else {
-            delete country.children
           }
         })
-        this.options = this.options.concat(countryMap)
-      })
+    }
   }
 }
 </script>
