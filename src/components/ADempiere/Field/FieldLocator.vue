6393edbbe470f0a5089341a9c26482024e890053
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2020-05-07 16:55:50 -0400

    feat: Add Locator Warehouse Field.

---
+++ src/components/ADempiere/Field/FieldLocator.vue
@@ -0,0 +1,109 @@
+<template>
+  <el-cascader
+    :ref="metadata.columnName"
+    v-model="value"
+    :placeholder="metadata.help"
+    :options="options"
+    :readonly="Boolean(metadata.readonly)"
+    :disabled="isDisabled"
+    :clearable="true"
+    filterable
+    lazy
+    :show-all-levels="false"
+    :lazy-load="searchLocatorByWarehouse"
+    :props="props"
+    @change="preHandleChange"
+  />
+</template>
+
+<script>
+import { fieldMixin } from '@/components/ADempiere/Field/FieldMixin'
+import { getLocatorList } from '@/api/ADempiere/field/locator'
+
+export default {
+  name: 'FieldLocation',
+  mixins: [fieldMixin],
+  data() {
+    return {
+      value: [],
+      options: [],
+      props: {
+        // checkStrictly: true,
+        // emitPath: false,
+        lazy: true,
+        lazyLoad: this.searchLocatorByWarehouse
+      }
+    }
+  },
+  computed: {
+    warehouse() {
+      return this.$store.getters['user/getWarehouse']
+    },
+    warehousesList() {
+      const list = this.$store.getters['user/getWarehouses']
+        .map(itemWarehouse => {
+          return {
+            label: itemWarehouse.name,
+            value: itemWarehouse.id,
+            children: []
+          }
+        })
+      list.push({ label: 'HQ', value: 103, children: [] })
+      list.push({ label: 'Fertilizer', value: 50001, children: [] })
+      return list
+    }
+  },
+  // watch: {
+  //   valueModel(value) {
+  //     if (this.metadata.inTable) {
+  //       if (this.isEmptyValue(value)) {
+  //         value = undefined
+  //       }
+  //       this.value = value
+  //     }
+  //   },
+  //   'metadata.value'(value) {
+  //     if (!this.metadata.inTable) {
+  //       if (this.isEmptyValue(value)) {
+  //         value = undefined
+  //       }
+  //       this.value = value
+  //     }
+  //   }
+  // },
+  created() {
+    this.options = this.warehousesList
+  },
+  methods: {
+    preHandleChange(value) {
+      let selected = value
+      if (Array.isArray(value)) {
+        selected = value[value.length - 1]
+      }
+      this.handleChange(selected)
+      this.value = value
+    },
+    searchLocatorByWarehouse(node, resolve) {
+      getLocatorList({
+        warehouseId: node.value
+      })
+        .then(responseData => {
+          const locatorList = responseData.recordsList.map(item => {
+            const { values } = item
+            return {
+              label: values.Value,
+              value: values.M_Locator_ID,
+              warehouse: values.M_Warehouse_ID, // node.value
+              leaf: true
+            }
+          })
+          resolve(locatorList)
+        })
+        .catch(error => {
+          console.warn(`Error getting Locator List By Warehouse from server. Code: ${error.code}. Message: ${error.message}.`)
+          resolve()
+        })
+    }
+  }
+}
+</script>
