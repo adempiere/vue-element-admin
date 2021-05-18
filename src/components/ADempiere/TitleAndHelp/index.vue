8f053628481ea9f5b3e6c9cf734c3d6bb3cf0898
EdwinBetanc0urt <EdwinBetanco0urt@outlook.com>
2021-05-17 22:50:05 -0400

    fix styles content.

--- src/components/ADempiere/TitleAndHelp/index.vue
+++ src/components/ADempiere/TitleAndHelp/index.vue
@@ -15,23 +15,25 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https:www.gnu.org/licenses/>.
 -->
+
 <template>
-  <h3 class="warn-content text-center">
+  <h3 class="title-header text-center">
     <el-popover
       v-if="!isEmptyValue(help)"
       ref="helpTitle"
       placement="top-start"
       :title="title"
+      :class="cssClassHelp"
       width="400"
       trigger="hover"
     >
-      <div v-html="help" />
+      <div class="content-help" v-html="help" />
     </el-popover>
 
     <el-button
       v-popover:helpTitle
       type="text"
-      :class="cssClassTitle + ' text-center'"
+      class="container-title text-center"
     >
       {{ title }}
     </el-button>
@@ -62,18 +64,63 @@ export default defineComponent({
       return props.name || root.$route.meta.title
     })
 
-    const cssClassTitle = computed(() => {
+    const cssClassHelp = computed(() => {
       if (root.$store.state.app.device === 'mobile') {
-        return 'custom-title-mobile'
+        return 'container-help-mobile'
       }
-      return 'custom-title'
+      return 'container-help'
     })
 
     return {
-      cssClassTitle,
+      cssClassHelp,
       title
     }
   }
 
 })
 </script>
+
+<style>
+  /* text content title */
+  .el-popover__title {
+    padding-top: 10px;
+    padding-left: 10px;
+  }
+</style>
+<style lang="scss" scoped>
+  // title container
+  .title-header {
+    margin: 0px 0px !important;
+    padding-top: 0px !important;
+  }
+
+  // text content help into popover
+  .content-help {
+    padding-right: 10px;
+    padding-left: 10px;
+    padding-top: 0px !important;
+    padding-bottom: 10px;
+    word-break: break-word;
+  }
+  // title of the main views into popover
+  .container-title {
+    text-align: center;
+    color: #000000;
+    // text-size-adjust: 20px;
+    font-size: 100%;
+    font-weight: 605 !important;
+  }
+
+  // smart browser help in title popover
+  .container-help {
+    width: 100%;
+    height: 200%;
+    padding-left: 39px !important;
+  }
+  // mobile smart browser help in title popover
+  .container-help-mobile {
+    width: 50%;
+    height: 50%;
+    padding-left: 15px !important;
+  }
+</style>
