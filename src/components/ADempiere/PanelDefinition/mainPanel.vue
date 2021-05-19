2f335190d9aab8c94bc25bc5e41bd54f7a8973dc
EdwinBetanc0urt <EdwinBetanco0urt@outlook.com>
2021-05-19 07:00:47 -0400

    feat: Add panel definition

---
+++ src/components/ADempiere/PanelDefinition/mainPanel.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="panel" />
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    containerUuid: {
+      require: true
+    }
+  },
+  setup() {
+    let panel
+
+    return {
+      panel
+    }
+  }
+})
+</script>
