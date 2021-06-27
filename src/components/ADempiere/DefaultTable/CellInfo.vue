7d94dc27a616c0a26ad606a77ba251766a84be3d
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2021-06-26 22:03:56 -0400

    feat: Add default table to replace data table.

---
+++ src/components/ADempiere/DefaultTable/CellInfo.vue
@@ -0,0 +1,122 @@
+<template>
+  <el-tag
+    v-if="isDocumentStatus"
+    key="document-status"
+    :type="documentStatus"
+    disable-transitions
+  >
+    {{ displayedValue }}
+  </el-tag>
+
+  <span v-else key="info-value">
+    {{ displayedValue }}
+  </span>
+</template>
+
+<script>
+import { defineComponent, computed } from '@vue/composition-api'
+
+import { COLUMNS_NAME_DOCUMENT_STATUS, FIELDS_DECIMALS } from '@/utils/ADempiere/references.js'
+import { typeValue } from '@/utils/ADempiere/valueUtils.js'
+import { formatField, convertBooleanToTranslationLang } from '@/utils/ADempiere/valueFormat.js'
+
+export default defineComponent({
+  name: 'CellInfo',
+
+  props: {
+    fieldAttributes: {
+      type: Object,
+      required: true
+    },
+    dataRow: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  setup(props, { root }) {
+    const { columnName } = props.fieldAttributes
+    const fieldValue = props.dataRow[columnName]
+
+    const isDocumentStatus = computed(() => {
+      return COLUMNS_NAME_DOCUMENT_STATUS.includes(columnName)
+    })
+
+    const displayedValue = computed(() => {
+      return formatValue(props.dataRow, props.fieldAttributes)
+    })
+
+    const documentStatus = computed(() => {
+      return root.tagStatus(fieldValue)
+    })
+
+    const formatNumber = ({ displayType, number }) => {
+      let fixed = 0
+      // Amount, Costs+Prices, Number
+      if (FIELDS_DECIMALS.includes(displayType)) {
+        fixed = 2
+      }
+      return new Intl.NumberFormat().format(number.toFixed(fixed))
+    }
+
+    /**
+     * @param {object} row, row data
+     * @param {object} field, field with attributes
+     */
+    const formatValue = (row, field) => {
+      const { componentPath, displayColumnName, displayType } = field
+
+      let valueToShow
+      switch (componentPath) {
+        case 'FieldDate':
+        case 'FieldTime': {
+          let cell = fieldValue
+          if (typeValue(cell) === 'DATE') {
+            cell = cell.getTime()
+          }
+          // replace number timestamp value for date
+          valueToShow = formatField(cell, displayType)
+          break
+        }
+
+        case 'FieldNumber':
+          if (root.isEmptyValue(fieldValue)) {
+            valueToShow = undefined
+            break
+          }
+          valueToShow = formatNumber({
+            displayType,
+            number: fieldValue
+          })
+          break
+
+        case 'FieldSelect':
+          valueToShow = row[displayColumnName]
+          if (root.isEmptyValue(valueToShow) && fieldValue === 0) {
+            valueToShow = field.defaultValue
+            break
+          }
+          break
+
+        case 'FieldYesNo':
+          // replace boolean true-false value for 'Yes' or 'Not' ('Si' or 'No' for spanish)
+          valueToShow = convertBooleanToTranslationLang(fieldValue)
+          break
+
+        default:
+          valueToShow = fieldValue
+          break
+      }
+
+      return valueToShow
+    }
+
+    return {
+      // computeds
+      isDocumentStatus,
+      documentStatus,
+      displayedValue
+    }
+  }
+})
+</script>
